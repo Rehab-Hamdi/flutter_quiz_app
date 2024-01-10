b9f8ba372a07d4e3ed75a535fb0991065275ab73
rehab <rehab123selim@gmail.com>
2024-01-10 11:13:05 +0200

    some updates in start screen
    i put all items in the center of screen

--- lib/project_screens/start_screen.dart
+++ lib/project_screens/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/project_screens/quiz_screen.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -6,21 +7,24 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Container(
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width*0.65,
-              height: MediaQuery.of(context).size.height*0.65,
-              child: Image.asset('assets/images/flutter.png')),
-          OutlinedButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.start),
-              label: const Text('Start Quiz'))
-        ],
-      ),
-    ));
+        child: Scaffold(
+          body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width*0.65,
+                      height: MediaQuery.of(context).size.height*0.65,
+                      child: Image.asset('assets/images/flutter.png')),
+                  OutlinedButton.icon(
+                      onPressed: () {
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> QuizScreen()));
+                      },
+                      icon: const Icon(Icons.start),
+                      label: const Text('Start Quiz'))
+                ],
+            ),
+          ),
+        ));
   }
 }
